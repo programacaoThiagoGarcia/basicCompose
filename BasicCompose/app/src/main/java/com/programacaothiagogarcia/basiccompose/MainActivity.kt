267138046679946780c575417921d62cea45625e
ThiagoGarcia <thiago.garcia@itsector.pt>
2023-10-25 16:54:21 +0100

    Done Codelab

--- BasicCompose/app/src/main/java/com/programacaothiagogarcia/basiccompose/MainActivity.kt
+++ BasicCompose/app/src/main/java/com/programacaothiagogarcia/basiccompose/MainActivity.kt
@@ -1,36 +1,39 @@
 package com.programacaothiagogarcia.basiccompose
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animate
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
-import androidx.compose.foundation.gestures.FlingBehavior
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.programacaothiagogarcia.basiccompose.ui.OnBoardingElement
@@ -74,58 +77,123 @@ private fun MyApp(
 }
 
 @Composable
-private fun Greetings(names: List<String> = listOf("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16")) {
+private fun Greetings(
+    names: List<String> = listOf(
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "11",
+        "12",
+        "13",
+        "14",
+        "15",
+        "16"
+    )
+) {
     LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
         this.items(items = names) {
-            Greeting(name = it)
+            GreetingCard(name = it)
         }
     }
 }
 
 @Composable
-private fun Greeting(name: String) {
-    val expand = rememberSaveable {
+private fun GreetingCard(name: String) {
+    Card(
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        )
+    ) {
+        CardContent(name = name)
+    }
+}
+
+@Composable
+private fun CardContent(name: String) {
+    var expand by rememberSaveable {
         mutableStateOf(false)
     }
-    val extraPadding by animateDpAsState(if (expand.value) 48.0.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioHighBouncy,
-            stiffness = Spring.StiffnessLow
-        ),
-        label = "",
-    )
+    val context = LocalContext.current
 
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            ),
+        verticalAlignment = Alignment.Top,
     ) {
-        Row(modifier = Modifier.padding(24.dp), verticalAlignment = Alignment.Top) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                Text(text = "Número da linha")
-                Text(text = name)
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Número da linha", style = MaterialTheme.typography.bodyMedium)
+            Text(
+                text = name,
+                style = MaterialTheme.typography.headlineLarge.copy(fontWeight = FontWeight.ExtraBold)
+            )
+            if (expand) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
             }
-            ElevatedButton(
-                onClick = {
-                    expand.value = !expand.value
-                }, modifier = Modifier
-            ) {
-                val text = if (expand.value) "Show Less" else "Show More"
-                Text(text = text)
-            }
+
+        }
+        IconButton(
+            onClick = {
+                expand = !expand
+            }, modifier = Modifier
+        ) {
+            androidx.compose.material3.Icon(
+                imageVector = if (expand) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                contentDescription = if (expand) context.resources.getString(R.string.show_more) else context.resources.getString(
+                    R.string.show_less
+                )
+            )
         }
     }
 }
 
+/*
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
+
+
 fun GreetingPreview() {
     MyApp(modifier = Modifier.fillMaxSize())
 }
+*/
 
 
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun GreetingPreview() {
+    GreetingCard(name = "hello")
+}
 
 
--- BasicCompose/app/src/main/java/com/programacaothiagogarcia/basiccompose/ui/SecondScreen.kt
+++ BasicCompose/app/src/main/java/com/programacaothiagogarcia/basiccompose/ui/SecondScreen.kt
@@ -1,13 +1,16 @@
 package com.programacaothiagogarcia.basiccompose.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.programacaothiagogarcia.basiccompose.ui.theme.BasicComposeTheme
@@ -28,13 +31,14 @@ fun OnBoardingElement(modifier: Modifier = Modifier, onClick: () -> Unit) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = modifier
+        modifier = modifier.background(color = MaterialTheme.colorScheme.background)
+
     ) {
-        Text(text = "Welcome to the Basics Codelab!")
-        ElevatedButton(modifier = Modifier.padding(24.dp), onClick = {
+        Text(text = "Welcome to the Basics Codelab!", style = MaterialTheme.typography.titleLarge.copy( fontWeight = FontWeight.Bold), color = MaterialTheme.colorScheme.onPrimary)
+        ElevatedButton(modifier = Modifier.padding(24.dp),  onClick = {
             onClick()
         }) {
-            Text(text = "Continue")
+            Text(text = "Continue", style = MaterialTheme.typography.labelMedium)
         }
     }
 }
